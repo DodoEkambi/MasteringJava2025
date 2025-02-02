eb306c95e87ecc530715dbbb727b66dfc60ac0e1
DodoEkambi <gkhngul2@gmail.com>
2025-02-02 22:14:43 +0300

    hp_pc1 tarafından oluşturulan hello world uygulamasının klasör altına alınması işlemi gerçekleştirildi.

--- HelloCopilot.java
+++
@@ -1,7 +0,0 @@
-public class HelloCopilot {//bu sınıf, bir isim alıp "Hello, <name>!" döndüren greeting isimli metod içermelidir    .
-    public String greeting(String name) {
-        return "Hello, " + name + "!";
-    }
-
-
-}
